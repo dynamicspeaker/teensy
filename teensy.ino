59fd6c2da2d6f6f9583a5165a25b8f70c5a9ae02
Jacques <22794425@sun.ac.za>
2022-08-28 13:36:13 +0200

    Change to spdif input

--- teensy.ino
+++ teensy.ino
@@ -5,18 +5,18 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S2           i2s2_1;         //xy=379.6666564941406,494.5555419921875
-AudioInputUSB            usb_in;         //xy=411.6666564941406,430.5555419921875
-AudioMixer4              mixer2;         //xy=559.6666564941406,595.5555419921875
-AudioMixer4              mixer1;         //xy=709.6666564941406,437.5555419921875
-AudioMixer4              mixer_mono;     //xy=739.6666564941406,649.5555419921875
-AudioFilterStateVariable filter_lp;      //xy=886.6666564941406,591.5555419921875
-AudioFilterStateVariable filter2;        //xy=904.6666717529297,425.55555725097656
-AudioFilterStateVariable filter1;        //xy=913.6666717529297,495.55555725097656
-AudioAnalyzeNoteFrequency notefreq1;      //xy=1023.6666564941406,650.5555419921875
-AudioOutputI2S           i2s_out;        //xy=1114.6666564941406,426.5555419921875
-AudioConnection          patchCord1(i2s2_1, 0, mixer1, 0);
-AudioConnection          patchCord2(i2s2_1, 1, mixer2, 0);
+AsyncAudioInputSPDIF3    spdif_async1;   //xy=446.41976165771484,696.1728219985962
+AudioInputUSB            usb_in;         //xy=482.8641662597656,628.8394775390625
+AudioMixer4              mixer2;         //xy=630.8641662597656,793.8394775390625
+AudioMixer4              mixer1;         //xy=780.8641662597656,635.8394775390625
+AudioMixer4              mixer_mono;     //xy=810.8641662597656,847.8394775390625
+AudioFilterStateVariable filter_lp;      //xy=957.8641662597656,789.8394775390625
+AudioFilterStateVariable filter2;        //xy=975.8641662597656,623.8394775390625
+AudioFilterStateVariable filter1;        //xy=984.8641662597656,693.8394775390625
+AudioAnalyzeNoteFrequency notefreq1;      //xy=1094.8641662597656,848.8394775390625
+AudioOutputI2S           i2s_out;        //xy=1185.8641662597656,624.8394775390625
+AudioConnection          patchCord1(spdif_async1, 0, mixer1, 0);
+AudioConnection          patchCord2(spdif_async1, 1, mixer2, 0);
 AudioConnection          patchCord3(usb_in, 0, mixer1, 1);
 AudioConnection          patchCord4(usb_in, 1, mixer2, 1);
 AudioConnection          patchCord5(mixer2, 0, mixer_mono, 1);
@@ -28,9 +28,6 @@ AudioConnection          patchCord10(filter_lp, 0, notefreq1, 0);
 AudioConnection          patchCord11(filter2, 0, i2s_out, 0);
 AudioConnection          patchCord12(filter1, 2, i2s_out, 1);
 // GUItool: end automatically generated code
-
-
-
 
 unsigned long prevRun1 = 0;
 
